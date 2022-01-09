fe8eb6086b97527bf15f8a3b3994afbbac3a13eb
Keviweto-Yhoshu <89389070+Keviweto-Yhoshu@users.noreply.github.com>
2022-01-09 20:28:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function   father()   {
+    window.location="father.html";
+}  
+ 
+function    mother()  {
+    window.location="mother.html";
+}  
+
+function    sister()  {
+    window.location="sister.html";
+}  
+
+function  baby()    {
+    window.location="baby.html";
+}  
+ 
+function    brother()  {
+    window.location="Me.html";
+}  
